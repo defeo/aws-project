740ff6916fe41d829cc2f36714050df6f27f4f69
Luca De Feo <luca.defeo@gmail.com>
2016-02-02 23:15:00 +0000

    Updated template to use C9's composer

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-curl -sS https://getcomposer.org/installer | php
-php composer.phar config -g github-oauth.github.com fa7525e8f9e94549c13607b6cd110b9e3a3f1460
-php composer.phar install
+composer config -g github-oauth.github.com fa7525e8f9e94549c13607b6cd110b9e3a3f1460
+composer install
